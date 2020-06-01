7edddc66ac0932718bcc51d787606107e5fd5305
Robert De Leon <robedeleong@gmail.com>
2020-05-31 19:58:23 -0500

    Started project

---
+++ src/main/java/com/backend/fcfm/PiaBackEnd2Application.java
@@ -0,0 +1,13 @@
+package com.backend.fcfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiaBackEnd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiaBackEnd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/fcfm/PiaBackEnd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.fcfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiaBackEnd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
